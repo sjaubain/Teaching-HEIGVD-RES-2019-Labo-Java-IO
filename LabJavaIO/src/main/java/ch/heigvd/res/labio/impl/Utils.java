624cbc4dba0b8786a15a2776a4f1722da1572981
simon <simon.jobin@heig-vd.ch>
2019-03-10 15:57:14 +0100

    Completed Utils tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -3,24 +3,52 @@ package ch.heigvd.res.labio.impl;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to extract
+     * the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the argument does not
+     * contain any line separator, then the first element is an empty string.
+     */
+    public static String[] getNextLine(String lines) {
+
+        String returnedLines[] = new String[2];
+        String nextLine = "", remainingText = lines;
+        int inputLength = lines.length();
+
+        // Walk through lines
+        for(int i = 0; i < inputLength; ++i) {
+
+            if(lines.charAt(i) == '\r') {
+                // Look if '\r' is followed by '\n'
+                if(i != inputLength - 1 && lines.charAt(i + 1) == '\n') {
+                    nextLine = lines.substring(0, i + 2);
+                    remainingText = lines.substring(i + 2, inputLength);
+                    break;
+                } else {
+                    nextLine = lines.substring(0, i + 1);
+                    remainingText = lines.substring(i + 1, inputLength);
+                    break;
+                }
+            } else if(lines.charAt(i) == '\n') {
+                nextLine = lines.substring(0, i + 1);
+                remainingText = lines.substring(i + 1, inputLength);
+                break;
+            }
+        }
+
+        returnedLines[0] = nextLine;
+        returnedLines[1] = remainingText;
+
+        return returnedLines;
+    }
 
 }
