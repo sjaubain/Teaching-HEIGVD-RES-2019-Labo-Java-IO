c9115cfc2ca2ccb7fb6d97f6450d64b5b7edf65b
simon <simon.jobin@heig-vd.ch>
2019-03-10 12:33:25 +0100

    Completed tests with uppercaseFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -5,28 +5,34 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    private static final int toUpperCaseAsciiOffset = -32;
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str.toUpperCase(), off, len);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        String str = new String(cbuf);
+        this.write(str, off, len);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        // If c is a lower case letter
+        if(c >= 97 && c <= 122)
+            c -= toUpperCaseAsciiOffset;
+        
+        super.write(c);
+    }
 
 }
